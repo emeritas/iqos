29566a6a7d4f47a83e4cea9b6e13d8a36caa775c
DominykasDriu <dominykas.driukas@gmail.com>
2021-04-20 21:13:52 +0300

    I tried and  I failed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './pages/Home';
+import Search from './pages/Search';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
+      <Search />
       <Footer />
     </div>
   );
