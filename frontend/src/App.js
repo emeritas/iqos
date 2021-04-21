da16630024856815a85bfb33ffd09b0ed9e33213
DominykasDriu <dominykas.driukas@gmail.com>
2021-04-21 19:09:22 +0300

    Started search page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <Header />
       {/* <Home /> */}
-      <About />
+      {/* <About /> */}
       <Search />
       <Footer />
     </div>
--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -4,12 +4,52 @@ import SelectComp from '../components/SelectComp'
 export default function Search() {
 
   return (
-    <main>
-      <SelectComp data={{
-        defaultOption: 'Pasirinkite savivaldybe!',
-        options: ['gargzdai', 'kaunas']
-      }}/>
-      <input className='form-control' type="text"/>
+      <main>
+      <section className="hero">
+        <div className="px-4 py-5 my-5 text-center">
+          <h1 className="display-5 fw-bold mb-5">Ieškokite jus dominančios įstaigos.</h1>
+          <div className="col-lg-6 mx-auto">
+            
+            <form className='' action="">
+              <div className="row g-4">
+                <div className="col-6">
+                  <input className='form-control' type="text" placeholder='Įstaigos pavadinimas'/>
+                </div>
+                <div className="col-6">
+                  <SelectComp data={{
+                  defaultOption: 'Pasirinkite savivaldybe!',
+                  options: ['gargzdai', 'kaunas']
+                  }}/>
+                </div>
+                <div className="col-6">
+                  <input className='form-control' type="text" placeholder='Valstybinis kodas'/>
+                </div>
+                <div className="col-6">
+                  <SelectComp data={{
+                  defaultOption: 'Pasirinkite savivaldybe!',
+                  options: ['gargzdai', 'kaunas']
+                  }}/>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
+      <section className="blue-bg">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm">
+              One of three columns
+            </div>
+            <div className="col-sm">
+              One of three columns
+            </div>
+            <div className="col-sm">
+              One of three columns
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
