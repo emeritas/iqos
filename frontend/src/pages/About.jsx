1c05da6202286cfec8c2f490ba2507f80d35c708
Ju-chan <75089351+Ju-chan@users.noreply.github.com>
2021-04-27 22:54:07 +0300

    Revert "About page ir API docs"
    This reverts commit addf09a3603c76fc4031f5086089f0e38a5c996c.

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -4,6 +4,31 @@ import Education from '../assets/images/education.jpg';
 export default function About() {
   return (
     <main className='fade-animation'>
+<<<<<<< Updated upstream
+      <section className='bg-light-blue pt-3 pb-3'>
+        <div className='container'>
+          <h1 className='main-title text-center font-bold'>Apie IQOS</h1>
+        </div>
+      </section>
+      <section>
+        <div className='container about-container'>
+          <img src={Education} className='about-img text-center' />
+
+          <div className='centered'>
+            <div className='about-page'>
+              <p>
+                IQOS komanda susibūrė 2005 m., kai tuometiniams abiturientams,
+                besiplaškantiems aukštųjų mokyklų pasirinkimų sukūryje, užgimė
+                idėja sukurti svetainę, kurioje organizuotai būtų pateikta
+                esminė informacija apie visas Lietuvoje veikiančias mokymo
+                įstaigas. Tuo metu svetainė startavo su 57 mokyklų sąrašu, dabar
+                svetainėje rasite virš 4000 institucijų.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+=======
       <section className='bg-light-grey'>
         <div className='container about-container'>
           <img src={HeadImg} className='about-img text-center' />
@@ -20,6 +45,7 @@ export default function About() {
       </section>
 
       <section></section>
+>>>>>>> Stashed changes
     </main>
   );
 }
--- frontend/src/pages/ApiDocs.jsx
+++ frontend/src/pages/ApiDocs.jsx
@@ -21,11 +21,19 @@ const ApiDocs = () => {
 
   return (
     <main style={mainStyle}>
+<<<<<<< Updated upstream
+      <div className='accordion accordion-flush' id='accordionFlushExample'>
+        <div className='accordion-item'>
+          <h2 className='accordion-header' id='flush-headingOne'>
+            <button
+              className='accordion-button collapsed'
+=======
       <div class='accordion accordion-flush' id='accordionFlushExample'>
         <div class='accordion-item'>
           <h2 class='accordion-header' id='flush-headingOne'>
             <button
               class='accordion-button collapsed'
+>>>>>>> Stashed changes
               type='button'
               data-bs-toggle='collapse'
               data-bs-target='#flush-collapseOne'
@@ -37,11 +45,19 @@ const ApiDocs = () => {
           </h2>
           <div
             id='flush-collapseOne'
+<<<<<<< Updated upstream
+            className='accordion-collapse collapse'
+            aria-labelledby='flush-headingOne'
+            data-bs-parent='#accordionFlushExample'
+          >
+            <div className='accordion-body'>
+=======
             class='accordion-collapse collapse'
             aria-labelledby='flush-headingOne'
             data-bs-parent='#accordionFlushExample'
           >
             <div class='accordion-body'>
+>>>>>>> Stashed changes
               <h2>Administratoriaus prisijungimas</h2>
               <pre style={preStyle}>
                 <h3>POST</h3>
